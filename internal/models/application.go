7c28d2a66ab7e1ac07061123bb2ad902e4401a1e
urcop <kirillzama@list.ru>
2024-05-15 22:26:01 +0300

    hotfix: update fields

--- internal/models/application.go
+++ internal/models/application.go
@@ -1,14 +1,24 @@
 package models
 
+import "time"
+
 type Application struct {
-	Id              uint    `json:"id" gorm:"primaryKey"`
-	Title           string  `json:"title"`
-	Status          string  `json:"status"`
-	Priority        string  `json:"priority"`
-	PerformerId     string  `json:"performer"`
-	Comment         *string `json:"comment"`
-	ApplicantId     string  `json:"applicant"`
-	ExecutionPeriod string  `json:"execution_period"`
-	CreatedAt       string  `json:"create_date"`
-	UpdatedAt       string  `json:"updated_at"`
+	Id              uint      `json:"id" gorm:"primaryKey"`
+	Title           string    `json:"title"`
+	Status          status    `json:"status"`
+	Priority        string    `json:"priority"`
+	PerformerId     string    `json:"performer"`
+	Comment         *string   `json:"comment"`
+	ApplicantId     string    `json:"applicant"`
+	ExecutionPeriod string    `json:"execution_period"`
+	CreatedAt       time.Time `json:"create_date"`
+	UpdatedAt       time.Time `json:"updated_at"`
 }
+
+type status string
+
+const (
+	InProgress status = "in_progress"
+	Pending    status = "pending"
+	Executed   status = "executed"
+)
