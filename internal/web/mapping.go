090ada3ee97a977609739956a557c1e082a5523b
urcop <kirillzama@list.ru>
2024-05-21 19:59:28 +0300

    feature: update applications response

--- internal/web/mapping.go
+++ internal/web/mapping.go
@@ -1,6 +1,7 @@
 package web
 
 import (
+	"github.com/SiriusServiceDesk/application-service/internal/grpc/client"
 	"github.com/SiriusServiceDesk/application-service/internal/helpers"
 	"github.com/SiriusServiceDesk/application-service/internal/models"
 	"sort"
@@ -46,6 +47,7 @@ func MappingApplicationsForUser(applications []*models.Application) []GetApplica
 }
 
 func MappingApplicationForAdmin(application *models.Application) GetApplicationResponse {
+	user, _ := client.GetUserById(application.ApplicantId)
 	return GetApplicationResponse{
 		Id:              helpers.FormatIdFromUintToString(application.Id),
 		Title:           application.Title,
@@ -53,7 +55,7 @@ func MappingApplicationForAdmin(application *models.Application) GetApplicationR
 		Performer:       application.PerformerId,
 		Priority:        application.Priority,
 		Comment:         application.Comment,
-		ApplicantId:     application.ApplicantId,
+		ApplicantId:     user.GetName() + " " + user.GetSurname(),
 		ExecutionPeriod: application.ExecutionPeriod,
 		FeedBack:        application.FeedBack,
 		CreatedAt:       helpers.FormatDate(application.CreatedAt),
@@ -63,6 +65,7 @@ func MappingApplicationForAdmin(application *models.Application) GetApplicationR
 func MappingApplicationsForAdmin(applications []*models.Application) []GetApplicationResponse {
 	var result []GetApplicationResponse
 	for _, application := range applications {
+		user, _ := client.GetUserById(application.ApplicantId)
 		result = append(result, GetApplicationResponse{
 			Id:              helpers.FormatIdFromUintToString(application.Id),
 			Title:           application.Title,
@@ -70,7 +73,7 @@ func MappingApplicationsForAdmin(applications []*models.Application) []GetApplic
 			Performer:       application.PerformerId,
 			Priority:        application.Priority,
 			Comment:         application.Comment,
-			ApplicantId:     application.ApplicantId,
+			ApplicantId:     user.GetName() + " " + user.GetSurname(),
 			ExecutionPeriod: application.ExecutionPeriod,
 			FeedBack:        application.FeedBack,
 			CreatedAt:       helpers.FormatDate(application.CreatedAt),
